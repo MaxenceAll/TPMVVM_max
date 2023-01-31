a164fae62ee1bc96c5c0a3da3cb8ad3baf02706a
Maxence <maxence.allart@gmail.com>
2023-01-31 16:22:39 +0100

    category view + fix if category saisie bidon

--- src/controllers/CategoryController.js
+++ src/controllers/CategoryController.js
@@ -1,23 +1,37 @@
 export class CategoryController{
 
     constructor(params){
-        this.title = params.pop();
+        this.categoryName = params?.pop()?.replace('-', ' ');
     }
 
     async execute(){
-        this.title = this.title.toLowerCase();
-        //models
+
+        this.categoryName = this.categoryName.toLowerCase();
+        //Models
         let resp = await fetch("../src/data/theme.json");
-        const themes = await resp.json();
-        const theme = themes.find(t => t.title.toLowerCase() == this.title);
+        const categories = await resp.json();                
+        const category = categories.find(c => c.title.toLowerCase() == this.categoryName);
 
-        resp = await fetch("../src/data/product.json");
-        const products = await resp.json();
-
-        // view
-        const {CategoryView} = await import('../views/CategoryView.js');
-        const view = new CategoryView(theme,products);
-        const content = view.render();
-        return content;
+        if (category==undefined){
+            const resp = await fetch('http://tpmvvm.loc/src/views/NotFoundView.html');
+            const view = await resp.text();
+            return view;
+        }
+        else{            
+            resp = await fetch("../src/data/product.json");
+            let products = await resp.json();
+            
+            products = products.filter(p => p.theme_id == category.id);
+            
+            //View
+            
+            const {CategoryView} = await import('../views/CategoryView.js');
+            const view = new CategoryView({category, products});
+            const content = view.render();
+            return content;
+        }
     }
 }
+
+
+
--- src/views/CategoryView.js
+++ src/views/CategoryView.js
@@ -1,26 +1,64 @@
 export class CategoryView {
-  
-    constructor(theme,products) {
-      this.theme = theme;
-      this.products = products;
-    }
-  
-    render = () => {
-
-      
-
-      return `
-      <div id="CategoryDetailView">
-
-      <link rel="stylesheet" href="../css/CategoryView.css">
-
-      <h1>Vous êtes sur la catégorie : ${this.theme.title}</h1>
-
-      <p>
-        ${this.products}
-      </p>
-
-      </div>
-      `;
-    };
+  constructor(models) {
+    this.models = models;
   }
+
+  render = () => {
+    const { category, products } = this.models;
+
+
+
+    const productsCards = products
+      .map((product) => {
+        return `
+
+
+        <div class="card p-0 m-2" onclick="location.href='/product/${product.id}'">
+
+            <img src="${product.img_src}" class="w-100" alt=${product.title}">
+
+            <div class="card-body">
+              <h5 class="card-title fw-bold text-decoration-underline">${product.title}</h5>
+              <h6 class="card-text">${product.content.substring(0,50)}...</h6>
+
+            </div>
+            <div class="card-footer justify-content-between">
+              <button type="button" class="btn btn-warning">Voir +</button>          
+            </div>
+        </div>
+
+        
+        `;
+      })
+      .join("");
+
+    return `
+        <div id="CategoryDetailView">
+        <link rel="stylesheet" href="../css/CategoryView.css">
+
+          <h1><u>Vous êtes sur la catégorie : ${category.title} </u></h1>
+            <div class="container-fluid">
+              <div class="row row-cols-4 justify-content-center">
+                  ${productsCards}
+              </div>
+            </div>
+        </div>
+
+        `;
+  };
+
+  //   return `
+  //   <div id="CategoryDetailView">
+
+  //   <link rel="stylesheet" href="../css/CategoryView.css">
+
+  //   <h1>Vous êtes sur la catégorie : ${this.category.title}</h1>
+
+  //   <p>
+  //     ${this.products}
+  //   </p>
+
+  //   </div>
+  //   `;
+  // };
+}
