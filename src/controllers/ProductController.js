9f5297d6f9db7cd6f321b8e95ca67b6282f09bb9
Maxence <maxence.allart@gmail.com>
2023-01-31 11:37:35 +0100

    ajout product view

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,8 +1,25 @@
 export class ProductController{
 
+    constructor(params){
+        this.id = params.pop();
+    }
+
     async execute(){
-        const resp = await fetch('http://tpmvvm.loc/src/views/ProductView.html');
-        const view = await resp.text();
-        return view;
+        //Models
+        let resp = await fetch("../src/data/product.json");
+        const products = await resp.json();
+        const product = products.find(p => p.id == this.id);
+
+        resp = await fetch("../src/data/theme.json");
+        const themes = await resp.json();
+        const theme = themes.find(t => t.id == product.theme_id);
+
+        //View
+        const {ProductView} = await import('../views/ProductView.js');
+        const view = new ProductView(product, theme);
+        const content = view.render();       
+
+
+        return content;
     }
 }
